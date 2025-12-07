aad9c1358a766419d7e5c347c58a95c171ccbaf2
Ashish Mule <ashishmule707@gmail.com>
2025-12-07 22:30:46 +0530

    added window

--- HelloWorld.tsx
+++ HelloWorld.tsx
@@ -1,17 +1,24 @@
 import React from "react";
 import { PluginComponentProps } from "@/types/plugins";
+import { WindowWithoutSideMenu } from "@/plugins/sdk";
 
-const HelloWorld: React.FC<PluginComponentProps> = ({ api, settings }) => {
+const HelloWorld: React.FC<PluginComponentProps> = ({ api, settings, app}) => {
   return (
-    <div className="p-4 bg-neutral-800 rounded-lg">
-      <h2 className="text-white text-xl">Hello World!</h2>
-      <button 
-        onClick={() => api.showToast("Hello from plugin!", "success")}
-        className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
-      >
-        Click Me
-      </button>
-    </div>
+    <WindowWithoutSideMenu
+      app={app}
+      defaultSize={{ width: 400, height: 600 }}
+      isResizable={false}
+    >
+      <div className="p-4 bg-neutral-800 rounded-lg">
+        <h2 className="text-white text-xl">Hello World!</h2>
+        <button
+          onClick={() => api.showToast("Hello from plugin!", "success")}
+          className="mt-2 px-4 py-2 bg-blue-500 text-white rounded"
+        >
+          Click Me
+        </button>
+      </div>
+    </WindowWithoutSideMenu>
   );
 };
 
